7119626e5944df99c47ec9c3e10da19cba3bf88b
ilslv <ilya.solovyiov@gmail.com>
2020-08-17 15:17:04 +0300

    Step 1.1 done

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,39 @@
+#[derive(Clone, Copy, Default, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone, Debug)]
+struct Polyline {
+    pub points: Vec<Point>,
+}
+
+impl Polyline {
+    pub fn new(point: Point) -> Polyline {
+        Polyline {
+            points: vec![point],
+        }
+    }
+
+    pub fn from_vec(vec: Vec<Point>) -> Polyline {
+        Polyline {
+            points: vec,
+        }
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    println!("{:?}", Polyline::new(Point::default()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_constructors() {
+        assert_eq!(Polyline::new(Point::default()).points.len(), 1);
+        assert_eq!(Polyline::from_vec(vec![Point::default(), Point::default()]).points.len(), 2);
+    }
 }
