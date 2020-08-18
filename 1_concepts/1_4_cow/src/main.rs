c3d3acd27e0af83e5e3c626fe1a685174de162f5
ilslv <ilya.solovyiov@gmail.com>
2020-08-18 17:14:24 +0300

    Do Step 1.4 without error thrown

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,18 @@
+use std::borrow::Cow;
+
+fn get_command_line_arg(name: &str) -> Option<String> {
+    //TODO: Panic if there is nothing after name parameter
+    std::env::args().skip_while(|arg| arg != name).nth(1)
+}
+
 fn main() {
-    println!("Implement me!");
+    let s: Cow<'static, str> = if let Ok(env_var) = std::env::var("APP_CONF") {
+        env_var.into()
+    } else if let Some(command_line_arg) = get_command_line_arg("--conf") {
+        command_line_arg.into()
+    } else {
+        "/etc/app/app.conf".into()
+    };
+
+    println!("{}", s);
 }
