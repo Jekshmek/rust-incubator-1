b3d4f3285ccf87e9dd275b12af454ff183761b8b
ilslv <ilya.solovyiov@gmail.com>
2020-08-21 21:07:48 +0300

    Add test for ListInternal

--- 1_concepts/src/main.rs
+++ 1_concepts/src/main.rs
@@ -1,7 +1,9 @@
 mod list {
     use std::cell::RefCell;
+    use std::fmt::Debug;
     use std::sync::{Arc, Weak};
 
+    #[derive(Debug)]
     struct Node<T> {
         data: T,
         next: Option<Arc<RefCell<Node<T>>>>,
@@ -17,26 +19,43 @@ mod list {
             }
         }
 
-        fn add_after(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
+        fn insert_after(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
             let new_node = Arc::new(RefCell::new(Node::new(data)));
 
-            if let Some(next) = &mut RefCell::borrow_mut(node).next {
-                RefCell::borrow_mut(next).prev = Some(Arc::downgrade(&new_node));
-                RefCell::borrow_mut(&new_node).next = Some(Arc::clone(next));
+            if let Some(next) = &mut node.borrow_mut().next {
+                next.borrow_mut().prev = Some(Arc::downgrade(&new_node));
+                new_node.borrow_mut().next = Some(Arc::clone(next));
             }
 
-            RefCell::borrow_mut(node).next = Some(new_node.clone());
+            node.borrow_mut().next = Some(new_node.clone());
+            new_node.borrow_mut().prev = Some(Arc::downgrade(node));
+
+            new_node
+        }
+
+        fn insert_before(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
+            let new_node = Arc::new(RefCell::new(Node::new(data)));
+
+            if let Some(prev) = &mut node.borrow_mut().prev {
+                let prev = prev.upgrade().unwrap();
+                prev.borrow_mut().next = Some(Arc::clone(&new_node));
+                new_node.borrow_mut().prev = Some(Arc::downgrade(&prev));
+            }
+
+            node.borrow_mut().prev = Some(Arc::downgrade(&new_node));
+            new_node.borrow_mut().next = Some(Arc::clone(node));
+
             new_node
         }
     }
 
-    #[derive(Default)]
+    #[derive(Default, Debug)]
     struct ListInternal<T> {
         first: Option<Arc<RefCell<Node<T>>>>,
         last: Option<Arc<RefCell<Node<T>>>>,
     }
 
-    impl<T> ListInternal<T> {
+    impl<T: Debug> ListInternal<T> {
         fn new() -> Self {
             ListInternal {
                 first: None,
@@ -44,58 +63,86 @@ mod list {
             }
         }
 
+        fn init(&mut self, data: T) {
+            let new_node = Arc::new(RefCell::new(Node::new(data)));
+            self.first = Some(new_node.clone());
+            self.last = Some(new_node);
+        }
+
         fn push_back(&mut self, data: T) {
-            if self.last.is_some() {
-                self.last = Some(Node::add_after(self.last.as_mut().unwrap(), data));
+            if let Some(last) = &mut self.last {
+                *last = Node::insert_after(last, data);
             } else {
-                let new_node = Arc::new(RefCell::new(Node::new(data)));
-                self.first = Some(new_node.clone());
-                self.last = Some(new_node);
+                self.init(data);
             }
         }
 
         fn push_front(&mut self, data: T) {
-            let mut new_node = Node::new(data);
-
-            new_node.next = self.first.clone();
-            self.first = Some(Arc::new(RefCell::new(new_node)));
-
-            if self.last.is_none() {
-                self.last = self.first.clone();
+            if let Some(first) = &mut self.first {
+                *first = Node::insert_before(first, data);
+            } else {
+                self.init(data);
             }
         }
 
         fn pop_back(&mut self) -> Option<T> {
-            let mut prev = self
-                .last
-                .as_ref()
-                .and_then(|last| RefCell::borrow(last).prev.clone())
-                .map(|prev| {
-                    let prev = prev.upgrade().unwrap();
-                    RefCell::borrow_mut(&prev).next = None;
-                    prev
-                });
+            if let Some(last) = &mut self.last {
+                let last = if let Some(prev) = &RefCell::borrow(&last.clone()).prev {
+                    *last = prev.upgrade().unwrap();
+                    Option::take(&mut last.borrow_mut().next).unwrap()
+                } else {
+                    self.first = None;
+                    Option::take(&mut self.last).unwrap()
+                };
 
-            std::mem::swap(&mut self.last, &mut prev);
+                return Arc::try_unwrap(last)
+                    .ok()
+                    .map(|node| node.into_inner().data);
+            }
 
-            prev.and_then(|node| Arc::try_unwrap(node).ok())
-                .map(|ref_cell| ref_cell.into_inner().data)
+            None
         }
 
         fn pop_front(&mut self) -> Option<T> {
-            let mut next = self
-                .first
-                .as_ref()
-                .and_then(|first| RefCell::borrow(first).next.clone())
-                .map(|next| {
-                    RefCell::borrow_mut(&next).prev = None;
-                    next
-                });
+            if let Some(first) = &mut self.first {
+                let res = if let Some(next) = &RefCell::borrow(&first.clone()).next {
+                    let res = first.clone();
+                    *first = next.clone();
+                    res
+                } else {
+                    self.last = None;
+                    Option::take(&mut self.first).unwrap()
+                };
 
-            std::mem::swap(&mut self.first, &mut next);
+                return Arc::try_unwrap(res).ok().map(|node| node.into_inner().data);
+            }
 
-            next.and_then(|node| Arc::try_unwrap(node).ok())
-                .map(|ref_cell| ref_cell.into_inner().data)
+            None
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn push_pop_test() {
+            let mut list = ListInternal::new();
+            list.push_back(2);
+            list.push_front(1);
+            list.push_back(3);
+
+            assert_eq!(list.pop_front(), Some(1));
+            assert_eq!(list.pop_front(), Some(2));
+            assert_eq!(list.pop_front(), Some(3));
+
+            list.push_front(2);
+            list.push_back(3);
+            list.push_front(1);
+
+            assert_eq!(list.pop_back(), Some(3));
+            assert_eq!(list.pop_back(), Some(2));
+            assert_eq!(list.pop_back(), Some(1));
         }
     }
 }
