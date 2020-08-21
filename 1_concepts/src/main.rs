e649fa7ffa36d706feda8194f59ac5d408c18acf
ilslv <ilya.solovyiov@gmail.com>
2020-08-21 17:47:30 +0300

    Add push_front and pop_back

--- 1_concepts/src/main.rs
+++ 1_concepts/src/main.rs
@@ -1,14 +1,11 @@
-use crate::list::List;
-
 mod list {
     use std::cell::RefCell;
-    use std::ops::Deref;
-    use std::sync::{Arc, RwLock, RwLockReadGuard, Weak};
+    use std::sync::{Arc, Weak};
 
     struct Node<T> {
         data: T,
-        next: Option<Arc<RwLock<Node<T>>>>,
-        prev: Option<Weak<RwLock<Node<T>>>>,
+        next: Option<Arc<RefCell<Node<T>>>>,
+        prev: Option<Weak<RefCell<Node<T>>>>,
     }
 
     impl<T> Node<T> {
@@ -20,23 +17,23 @@ mod list {
             }
         }
 
-        fn add_after(node: &mut Arc<RwLock<Node<T>>>, data: T) -> Arc<RwLock<Node<T>>> {
-            let new_node = Arc::new(RwLock::new(Node::new(data)));
+        fn add_after(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
+            let new_node = Arc::new(RefCell::new(Node::new(data)));
 
-            if let Some(next) = &RwLock::read(&node).unwrap().next {
-                RwLock::write(next).unwrap().prev = Some(Arc::downgrade(&new_node));
-                RwLock::write(&new_node).unwrap().next = Some(Arc::clone(next));
+            if let Some(next) = &mut RefCell::borrow_mut(node).next {
+                RefCell::borrow_mut(next).prev = Some(Arc::downgrade(&new_node));
+                RefCell::borrow_mut(&new_node).next = Some(Arc::clone(next));
             }
 
-            RwLock::write(node).unwrap().next = Some(new_node.clone());
+            RefCell::borrow_mut(node).next = Some(new_node.clone());
             new_node
         }
     }
 
     #[derive(Default)]
     struct ListInternal<T> {
-        first: Option<Arc<RwLock<Node<T>>>>,
-        last: Option<Arc<RwLock<Node<T>>>>,
+        first: Option<Arc<RefCell<Node<T>>>>,
+        last: Option<Arc<RefCell<Node<T>>>>,
     }
 
     impl<T> ListInternal<T> {
@@ -47,105 +44,44 @@ mod list {
             }
         }
 
-        fn append(&mut self, data: T) {
+        fn push_back(&mut self, data: T) {
             if self.last.is_some() {
                 self.last = Some(Node::add_after(self.last.as_mut().unwrap(), data));
             } else {
-                let new_node = Arc::new(RwLock::new(Node::new(data)));
+                let new_node = Arc::new(RefCell::new(Node::new(data)));
                 self.first = Some(new_node.clone());
                 self.last = Some(new_node);
             }
         }
-    }
 
-    #[derive(Default)]
-    pub struct List<T>(RwLock<ListInternal<T>>);
+        fn push_front(&mut self, data: T) {
+            let mut new_node = Node::new(data);
 
-    impl<T> List<T> {
-        pub fn new() -> Self {
-            List(RwLock::new(ListInternal::new()))
-        }
+            new_node.next = self.first.clone();
+            self.first = Some(Arc::new(RefCell::new(new_node)));
 
-        pub fn append(&self, data: T) {
-            let mut list = self.0.write().unwrap();
-            list.append(data);
-        }
-    }
-
-    impl<'a, T> IntoIterator for &'a List<T> {
-        type Item = ListItem<'a, T>;
-        type IntoIter = ListIter<'a, T>;
-
-        fn into_iter(self) -> Self::IntoIter {
-            ListIter {
-                list: self,
-                node: self.0.read().unwrap().first.clone(),
+            if self.last.is_none() {
+                self.last = self.first.clone();
             }
         }
-    }
 
-    pub struct ListIter<'a, T> {
-        list: &'a List<T>,
-        node: Option<Arc<RwLock<Node<T>>>>,
-    }
+        fn pop_back(&mut self) -> Option<T> {
+            let mut prev = self
+                .last
+                .as_ref()
+                .and_then(|last| RefCell::borrow_mut(last).prev.clone())
+                .map(|prev| {
+                    let prev = prev.upgrade().unwrap();
+                    RefCell::borrow_mut(&prev).next = None;
+                    prev
+                });
 
-    impl<'a, T> Iterator for ListIter<'a, T> {
-        type Item = ListItem<'a, T>;
+            std::mem::swap(&mut self.last, &mut prev);
 
-        fn next(&mut self) -> Option<Self::Item> {
-            if self.node.is_some() {
-                let list = self.list.0.read().unwrap();
-                let data = self.node.as_ref().unwrap().clone();
-
-                self.node = self
-                    .node
-                    .as_ref()
-                    .map(|n| RwLock::read(&n).unwrap().next.clone())
-                    .flatten();
-
-                return Some(ListItem { list, data });
-            }
-
-            None
-        }
-    }
-
-    pub struct ListItem<'a, T> {
-        list: RwLockReadGuard<'a, ListInternal<T>>,
-        data: Arc<RwLock<Node<T>>>,
-    }
-
-    impl<'a, T> ListItem<'a, T> {
-        pub fn peek(&self, f: impl FnOnce(&T)) {
-            f(&RwLock::read(&self.data).unwrap().data);
+            prev.and_then(|node| Arc::try_unwrap(node).ok())
+                .map(|ref_cell| ref_cell.into_inner().data)
         }
     }
 }
 
-fn main() {
-    let list = List::new();
-
-    for i in 1..10 {
-        list.append(i);
-    }
-
-    crossbeam::scope(|scope| {
-        scope.spawn(|_| {
-            for i in &list {
-                std::thread::sleep(core::time::Duration::from_secs(1));
-                i.peek(|n| println!("{}", n));
-            }
-        });
-
-        scope.spawn(|_| {
-            for i in 11..20 {
-                list.append(i);
-            }
-        });
-    })
-    .unwrap();
-
-    for i in &list {
-        i.peek(|n| println!("{}", n));
-    }
-}
+fn main() {}
