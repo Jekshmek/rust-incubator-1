6d7d6f87e14c33a08c0d5d8f423eb0bb4e5ee948
ilslv <ilya.solovyiov@gmail.com>
2020-08-22 22:41:11 +0300

    Final implementation

--- 1_concepts/src/main.rs
+++ 1_concepts/src/main.rs
@@ -1,11 +1,12 @@
-mod list {
-    use std::cell::{Ref, RefCell};
-    use std::sync::{Arc, Mutex, MutexGuard, Weak};
+use crate::list::List;
 
-    pub struct Node<T> {
+mod list {
+    use std::sync::{Arc, Mutex, Weak};
+
+    struct Node<T> {
         data: T,
-        next: Option<Arc<RefCell<Node<T>>>>,
-        prev: Option<Weak<RefCell<Node<T>>>>,
+        next: Option<Arc<Mutex<Node<T>>>>,
+        prev: Option<Weak<Mutex<Node<T>>>>,
     }
 
     impl<T> Node<T> {
@@ -17,50 +18,50 @@ mod list {
             }
         }
 
-        fn insert_after(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
-            let new_node = Arc::new(RefCell::new(Node::new(data)));
+        fn insert_after(node: &mut Arc<Mutex<Node<T>>>, data: T) -> Arc<Mutex<Node<T>>> {
+            let new_node = Arc::new(Mutex::new(Node::new(data)));
 
-            if let Some(next) = &mut RefCell::borrow_mut(node).next {
-                RefCell::borrow_mut(next).prev = Some(Arc::downgrade(&new_node));
-                RefCell::borrow_mut(&new_node).next = Some(Arc::clone(next));
+            if let Some(next) = &mut node.lock().unwrap().next {
+                next.lock().unwrap().prev = Some(Arc::downgrade(&new_node));
+                new_node.lock().unwrap().next = Some(Arc::clone(next));
             }
 
-            RefCell::borrow_mut(node).next = Some(new_node.clone());
-            RefCell::borrow_mut(&new_node).prev = Some(Arc::downgrade(node));
+            node.lock().unwrap().next = Some(new_node.clone());
+            new_node.lock().unwrap().prev = Some(Arc::downgrade(node));
 
             new_node
         }
 
-        fn insert_before(node: &mut Arc<RefCell<Node<T>>>, data: T) -> Arc<RefCell<Node<T>>> {
-            let new_node = Arc::new(RefCell::new(Node::new(data)));
+        fn insert_before(node: &mut Arc<Mutex<Node<T>>>, data: T) -> Arc<Mutex<Node<T>>> {
+            let new_node = Arc::new(Mutex::new(Node::new(data)));
 
-            if let Some(prev) = &mut RefCell::borrow_mut(node).prev {
+            if let Some(prev) = &mut node.lock().unwrap().prev {
                 let prev = prev.upgrade().unwrap();
-                RefCell::borrow_mut(&prev).next = Some(Arc::clone(&new_node));
-                RefCell::borrow_mut(&new_node).prev = Some(Arc::downgrade(&prev));
+                prev.lock().unwrap().next = Some(Arc::clone(&new_node));
+                new_node.lock().unwrap().prev = Some(Arc::downgrade(&prev));
             }
 
-            RefCell::borrow_mut(node).prev = Some(Arc::downgrade(&new_node));
-            RefCell::borrow_mut(&new_node).next = Some(Arc::clone(node));
+            node.lock().unwrap().prev = Some(Arc::downgrade(&new_node));
+            new_node.lock().unwrap().next = Some(Arc::clone(node));
 
             new_node
         }
     }
 
     #[derive(Default)]
-    struct ListInternal<T> {
-        first: Option<Arc<RefCell<Node<T>>>>,
-        last: Option<Arc<RefCell<Node<T>>>>,
+    pub struct List<T> {
+        first: Option<Arc<Mutex<Node<T>>>>,
+        last: Option<Arc<Mutex<Node<T>>>>,
     }
 
-    impl<T> ListInternal<T> {
-        fn init(&mut self, data: T) {
-            let new_node = Arc::new(RefCell::new(Node::new(data)));
+    impl<T> List<T> {
+        pub fn init(&mut self, data: T) {
+            let new_node = Arc::new(Mutex::new(Node::new(data)));
             self.first = Some(new_node.clone());
             self.last = Some(new_node);
         }
 
-        fn push_back(&mut self, data: T) {
+        pub fn push_back(&mut self, data: T) {
             if let Some(last) = &mut self.last {
                 *last = Node::insert_after(last, data);
             } else {
@@ -68,7 +69,7 @@ mod list {
             }
         }
 
-        fn push_front(&mut self, data: T) {
+        pub fn push_front(&mut self, data: T) {
             if let Some(first) = &mut self.first {
                 *first = Node::insert_before(first, data);
             } else {
@@ -76,11 +77,11 @@ mod list {
             }
         }
 
-        fn pop_back(&mut self) -> Option<T> {
+        pub fn pop_back(&mut self) -> Option<T> {
             if let Some(last) = &mut self.last {
-                let last = if let Some(prev) = &RefCell::borrow_mut(&last.clone()).prev {
+                let last = if let Some(prev) = &last.clone().lock().unwrap().prev {
                     *last = prev.upgrade().unwrap();
-                    Option::take(&mut RefCell::borrow_mut(&last).next).unwrap()
+                    Option::take(&mut last.lock().unwrap().next).unwrap()
                 } else {
                     self.first = None;
                     Option::take(&mut self.last).unwrap()
@@ -88,103 +89,68 @@ mod list {
 
                 return Arc::try_unwrap(last)
                     .ok()
-                    .map(|node| node.into_inner().data);
+                    .map(|node| node.into_inner().unwrap().data);
             }
 
             None
         }
 
-        fn pop_front(&mut self) -> Option<T> {
+        pub fn pop_front(&mut self) -> Option<T> {
             if let Some(first) = &mut self.first {
-                let res = if let Some(next) = &RefCell::borrow_mut(&first.clone()).next {
+                let res = if let Some(next) = &first.clone().lock().unwrap().next {
                     let res = first.clone();
                     *first = next.clone();
-                    first.borrow_mut().prev = None;
+                    first.lock().unwrap().prev = None;
                     res
                 } else {
                     self.last = None;
                     Option::take(&mut self.first).unwrap()
                 };
 
-                return Arc::try_unwrap(res).ok().map(|node| node.into_inner().data);
+                return Arc::try_unwrap(res)
+                    .ok()
+                    .map(|node| node.into_inner().unwrap().data);
             }
 
             None
         }
 
-        fn iter(&self) -> ListIter<T> {
-            ListIter(self.first.as_ref().map(|node| RefCell::borrow(node)))
+        pub fn iter(&self) -> ListIter<T> {
+            self.into_iter()
         }
     }
 
-    pub struct ListIter<'a, T>(Option<Ref<'a, Node<T>>>);
-
-    impl<'a, T> Iterator for ListIter<'a, T> {
-        type Item = Ref<'a, T>;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let mut is_there_next = true;
-
-            if let Some(node) = &self.0 {
-                let next = unsafe {
-                    Ref::map(Ref::clone(node), |node| {
-                        if let Some(next) = node.next.as_ref() {
-                            RefCell::try_borrow_unguarded(next).unwrap()
-                        } else {
-                            is_there_next = false;
-                            node
-                        }
-                    })
-                };
-
-                let mut next = Some(next);
-
-                std::mem::swap(&mut self.0, &mut next);
-
-                if !is_there_next {
-                    self.0 = None;
-                }
-
-                return next.map(|node| Ref::map(node, |node| &node.data));
-            }
-
-            None
-        }
-    }
-
-    #[derive(Default)]
-    pub struct List<T>(Mutex<ListInternal<T>>);
-
-    impl<T> List<T> {
-        pub fn iter(&self) -> ListGuard<T> {
-            ListGuard(self.0.lock().unwrap())
-        }
-
-        pub fn push_back(&self, data: T) {
-            self.0.lock().unwrap().push_back(data);
-        }
-
-        pub fn push_front(&self, data: T) {
-            self.0.lock().unwrap().push_front(data);
-        }
-
-        pub fn pop_back(&self) -> Option<T> {
-            self.0.lock().unwrap().pop_back()
-        }
-
-        pub fn pop_front(&self) -> Option<T> {
-            self.0.lock().unwrap().pop_front()
-        }
-    }
-
-    pub struct ListGuard<'a, T>(MutexGuard<'a, ListInternal<T>>);
-
-    impl<'a, 'b: 'a, T> IntoIterator for &'b ListGuard<'a, T> {
-        type Item = Ref<'a, T>;
-        type IntoIter = ListIter<'a, T>;
+    impl<T> IntoIterator for &List<T> {
+        type Item = ListItem<T>;
+        type IntoIter = ListIter<T>;
 
         fn into_iter(self) -> Self::IntoIter {
-            self.0.iter()
+            ListIter(self.first.clone())
+        }
+    }
+
+    pub struct ListIter<T>(Option<Arc<Mutex<Node<T>>>>);
+
+    impl<T> Iterator for ListIter<T> {
+        type Item = ListItem<T>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let mut next = self
+                .0
+                .as_ref()
+                .and_then(|node| node.lock().unwrap().next.clone());
+
+            std::mem::swap(&mut self.0, &mut next);
+
+            next.map(ListItem)
+        }
+    }
+
+    pub struct ListItem<T>(Arc<Mutex<Node<T>>>);
+
+    impl<T> ListItem<T> {
+        pub fn peek<E, F: FnOnce(&mut T) -> E>(&self, f: F) -> E {
+            f(&mut self.0.lock().unwrap().data)
         }
     }
 
@@ -193,8 +159,8 @@ mod list {
         use super::*;
 
         #[test]
-        fn push_pop() {
-            let mut list = ListInternal::default();
+        fn push_pop_internal() {
+            let mut list = List::default();
 
             list.push_back(2);
             list.push_front(1);
@@ -216,18 +182,48 @@ mod list {
         }
 
         #[test]
-        fn iterator() {
+        fn thread_safety() {
             let mut list = List::default();
 
             list.push_back(0);
-            list.push_back(1);
-            list.push_back(2);
+            list.push_back(0);
+            list.push_back(0);
 
-            for (index, val) in IntoIterator::into_iter(&list.iter()).enumerate() {
-                assert_eq!(index, *val);
+            for (index, item) in list.iter().enumerate() {
+                crossbeam::scope(|scope| {
+                    scope.spawn(|_| {
+                        std::thread::sleep(core::time::Duration::from_millis(10));
+                        for item in &list {
+                            item.peek(|node| *node += 1);
+                        }
+                    });
+                })
+                .unwrap();
+
+                item.peek(|node| {
+                    let old = *node;
+                    std::thread::sleep(core::time::Duration::from_millis(30));
+                    let new = *node;
+                    assert_eq!(old, new);
+                    assert_eq!(new, index + 1);
+                });
+            }
+
+            for item in &list {
+                assert!(item.peek(|node| *node == 3));
             }
         }
     }
 }
 
-fn main() {}
+fn main() {
+    let mut list = List::default();
+
+    list.push_back(1);
+    list.push_front(0);
+    list.push_back(2);
+
+    for (index, item) in list.iter().enumerate() {
+        assert!(item.peek(|val| *val == index));
+    }
+}
