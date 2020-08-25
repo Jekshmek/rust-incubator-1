039d52c13412a91fca289748234dec8022b3b3ba
ilslv <ilya.solovyiov@gmail.com>
2020-08-25 14:06:37 +0300

    Add for out of u32 bounds

--- 3_ecosystem/3_1_testing/tests/guess_the_number.rs
+++ 3_ecosystem/3_1_testing/tests/guess_the_number.rs
@@ -9,3 +9,13 @@ fn empty_env_args() {
 
     assert!(!output.status.success());
 }
+
+#[test]
+fn too_big_number() {
+    let output = Command::new("cargo")
+        .args(&["run", "-p", "step_3_1", "--", &u64::MAX.to_string()])
+        .output()
+        .unwrap();
+
+    assert!(!output.status.success());
+}
