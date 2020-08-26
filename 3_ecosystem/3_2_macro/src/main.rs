c31570590f1367fa55d04675b20216252106ea13
ilslv <ilya.solovyiov@gmail.com>
2020-08-26 19:26:41 +0300

    Add trailing comma support for btreemap_decl macro

--- 3_ecosystem/3_2_macro/src/main.rs
+++ 3_ecosystem/3_2_macro/src/main.rs
@@ -2,7 +2,7 @@ extern crate step_3_2;
 use step_3_2::btreemap_proc;
 
 macro_rules! btreemap_decl {
-    ( $(( $key:expr, $val:expr )),* ) => {{
+    ( $(( $key:expr, $val:expr )),* $(,)?) => {{
         let mut map = std::collections::BTreeMap::new();
         $(
             map.insert($key, $val);
@@ -13,7 +13,7 @@ macro_rules! btreemap_decl {
 
 fn main() {
     let map_decl = btreemap_decl![(1, "one"), (2, "two")];
-    let map_proc = btreemap_proc![(1, "one"), (2, "two")];
+    let map_proc = btreemap_proc![(1, "one"), (2, "two"),];
 
     dbg!(&map_proc);
 
