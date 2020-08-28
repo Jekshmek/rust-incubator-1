aea36d2ae89bb50e0c2c880fa7417162b1cdf9da
ilslv <ilya.solovyiov@gmail.com>
2020-08-28 14:35:51 +0300

    Implement Duration serializer

--- 3_ecosystem/3_6_serde/src/main.rs
+++ 3_ecosystem/3_6_serde/src/main.rs
@@ -9,12 +9,26 @@ use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 struct PublicTariff<'a> {
     id: u64,
     price: u64,
-    #[serde(deserialize_with = "deserialize_duration")]
+    #[serde(deserialize_with = "de_duration", serialize_with = "se_duration")]
     duration: Duration,
     description: &'a str,
 }
 
-fn deserialize_duration<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+fn se_duration<S>(d: &Duration, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let res: String = match d.as_millis() {
+        x if x < 1000 => x.to_string() + "ms",
+        x if x < 60_000 => (x / 1000).to_string() + "s",
+        x if x < 3_600_000 => (x / 60_000).to_string() + "m",
+        x => (x / 3_600_000).to_string() + "h",
+    };
+
+    s.serialize_str(&res)
+}
+
+fn de_duration<'de, D>(deserializer: D) -> Result<Duration, D::Error>
 where
     D: Deserializer<'de>,
 {
@@ -39,9 +53,10 @@ impl<'de> Visitor<'de> for DurationVisitor {
         let discard_last2 = |s: &str| s[..s.len() - 2].parse::<u64>().map_err(de::Error::custom);
 
         match v {
+            v if v.ends_with("ms") => discard_last2(v).map(Duration::from_millis),
+            v if v.ends_with("s") => discard_last2(v).map(Duration::from_secs),
             v if v.ends_with('m') => discard_last1(v).map(|m| Duration::from_secs(m * 60)),
             v if v.ends_with('h') => discard_last1(v).map(|m| Duration::from_secs(m * 3600)),
-            v if v.ends_with("ms") => discard_last2(v).map(Duration::from_millis),
             _ => Err(de::Error::custom("Expected 'm', 'h' or 'ms'")),
         }
     }
@@ -67,29 +82,40 @@ mod tests {
 
     #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
     struct Dur {
-        #[serde(deserialize_with = "deserialize_duration")]
+        #[serde(deserialize_with = "de_duration", serialize_with = "se_duration")]
         duration: Duration,
     }
 
     #[test]
     fn duration_de() {
+        let str = r#"{"duration":"1m"}"#;
+        let d: Dur = serde_json::from_str(str).unwrap();
         assert_eq!(
             Dur {
                 duration: Duration::from_secs(60)
             },
-            serde_json::from_str(r#"{"duration":"1m"}"#).unwrap()
+            d
         );
+        assert_eq!(str, serde_json::to_string(&d).unwrap());
+
+        let str = r#"{"duration":"3h"}"#;
+        let d: Dur = serde_json::from_str(str).unwrap();
         assert_eq!(
             Dur {
                 duration: Duration::from_secs(3 * 3600)
             },
-            serde_json::from_str(r#"{"duration":"3h"}"#).unwrap()
+            d
         );
+        assert_eq!(str, serde_json::to_string(&d).unwrap());
+
+        let str = r#"{"duration":"300ms"}"#;
+        let d: Dur = serde_json::from_str(str).unwrap();
         assert_eq!(
             Dur {
                 duration: Duration::from_millis(300)
             },
-            serde_json::from_str(r#"{"duration":"300ms"}"#).unwrap()
+            d
         );
+        assert_eq!(str, serde_json::to_string(&d).unwrap());
     }
 }
