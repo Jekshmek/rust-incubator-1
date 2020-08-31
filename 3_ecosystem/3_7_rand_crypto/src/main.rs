23afc4efd8d5f351295a9ce89db100a19b618fee
ilslv <ilya.solovyiov@gmail.com>
2020-08-31 09:01:18 +0300

    Implement select_rand_val()

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,10 +1,15 @@
-use rand::seq::IteratorRandom;
+use rand::seq::{IteratorRandom, SliceRandom};
 
 fn generate_password(elements: &str, len: usize) -> Option<String> {
     let mut rng = rand::thread_rng();
     (0..len)
         .map(|_| elements.chars().choose(&mut rng))
         .collect()
+}
+
+fn select_rand_val<T>(slice: &[T]) -> Option<&T> {
+    let mut rng = rand::thread_rng();
+    slice.choose(&mut rng)
 }
 
 fn main() {
