a1754bb6700c1319760a3f3cd666f16d791ff041
ilslv <ilya.solovyiov@gmail.com>
2020-08-31 09:46:05 +0300

    Implement hash_password()

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,11 +1,14 @@
-use rand::distributions::Alphanumeric;
-use rand::seq::{IteratorRandom, SliceRandom};
-use rand::{Rng, SeedableRng};
-use rand_chacha::ChaCha8Rng;
-use sha3::{Digest, Sha3_256};
 use std::path::PathBuf;
 
-fn generate_password(elements: &str, len: usize) -> Option<String> {
+use rand::{
+    distributions::Alphanumeric,
+    seq::{IteratorRandom, SliceRandom},
+    Rng, SeedableRng,
+};
+use rand_chacha::ChaCha8Rng;
+use sha3::{Digest, Sha3_256};
+
+fn generate_password(elements: impl Into<&str>, len: usize) -> Option<String> {
     let mut rng = rand::thread_rng();
     (0..len)
         .map(|_| elements.chars().choose(&mut rng))
@@ -22,10 +25,15 @@ fn new_access_token() -> String {
     (0..64).map(|_| rng.sample(Alphanumeric)).collect()
 }
 
-fn get_file_hash(path: PathBuf) -> Option<Vec<u8>> {
+fn get_file_hash(path: impl Into<PathBuf>) -> Option<Vec<u8>> {
     let str = std::fs::read_to_string(path).ok()?;
     let hash = Sha3_256::digest(str.as_bytes()).to_vec();
     Some(hash)
+}
+
+fn hash_password(password: impl Into<&str>) -> String {
+    let mut hasher = argonautica::Hasher::default();
+    hasher.with_password(password).hash().unwrap()
 }
 
 fn main() {
