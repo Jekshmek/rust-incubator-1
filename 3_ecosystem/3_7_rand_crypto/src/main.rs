62d00bdb3e58157c6487d75122155a50904c4bf3
ilslv <ilya.solovyiov@gmail.com>
2020-08-31 08:58:36 +0300

    Implement generate_password()

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,3 +1,12 @@
+use rand::seq::IteratorRandom;
+
+fn generate_password(elements: &str, len: usize) -> Option<String> {
+    let mut rng = rand::thread_rng();
+    (0..len)
+        .map(|_| elements.chars().choose(&mut rng))
+        .collect()
+}
+
 fn main() {
     println!("Implement me!");
 }
