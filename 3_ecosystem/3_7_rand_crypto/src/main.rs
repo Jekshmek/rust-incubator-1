94eed5107c42b129892f7c640153113d4e9402ef
ilslv <ilya.solovyiov@gmail.com>
2020-08-31 09:38:41 +0300

    Implement get_file_hash()

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -2,6 +2,8 @@ use rand::distributions::Alphanumeric;
 use rand::seq::{IteratorRandom, SliceRandom};
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
+use sha3::{Digest, Sha3_256};
+use std::path::PathBuf;
 
 fn generate_password(elements: &str, len: usize) -> Option<String> {
     let mut rng = rand::thread_rng();
@@ -18,6 +20,12 @@ fn select_rand_val<T>(slice: &[T]) -> Option<&T> {
 fn new_access_token() -> String {
     let mut rng = ChaCha8Rng::from_entropy();
     (0..64).map(|_| rng.sample(Alphanumeric)).collect()
+}
+
+fn get_file_hash(path: PathBuf) -> Option<Vec<u8>> {
+    let str = std::fs::read_to_string(path).ok()?;
+    let hash = Sha3_256::digest(str.as_bytes()).to_vec();
+    Some(hash)
 }
 
 fn main() {
