8b4ff48407f41cd583262858008606a883bc0313
ilslv <ilya.solovyiov@gmail.com>
2020-08-31 10:11:00 +0300

    Add tests

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -8,10 +8,15 @@ use rand::{
 use rand_chacha::ChaCha8Rng;
 use sha3::{Digest, Sha3_256};
 
-fn generate_password(elements: impl Into<&str>, len: usize) -> Option<String> {
+fn generate_password<'a>(elements: impl Into<&'a str>, len: usize) -> String {
     let mut rng = rand::thread_rng();
-    (0..len)
-        .map(|_| elements.chars().choose(&mut rng))
+
+    elements
+        .into()
+        .chars()
+        .choose_multiple(&mut rng, len)
+        .to_vec()
+        .into_iter()
         .collect()
 }
 
@@ -26,16 +31,27 @@ fn new_access_token() -> String {
 }
 
 fn get_file_hash(path: impl Into<PathBuf>) -> Option<Vec<u8>> {
-    let str = std::fs::read_to_string(path).ok()?;
+    let str = std::fs::read_to_string(path.into()).ok()?;
     let hash = Sha3_256::digest(str.as_bytes()).to_vec();
     Some(hash)
 }
 
-fn hash_password(password: impl Into<&str>) -> String {
+fn hash_password<'a>(password: impl Into<&'a str>) -> String {
     let mut hasher = argonautica::Hasher::default();
-    hasher.with_password(password).hash().unwrap()
+    hasher
+        .with_password(password.into())
+        .opt_out_of_secret_key(true)
+        .hash()
+        .unwrap()
 }
 
 fn main() {
-    println!("Implement me!");
+    println!("{}", generate_password("abcd", 5));
+    println!("{}", select_rand_val(&[1, 2, 3, 4]).unwrap());
+    println!("{}", new_access_token());
+    println!("{}", hash_password("password"));
+
+    let mut path = std::env::current_dir().unwrap();
+    path.push("README.md");
+    dbg!(get_file_hash(path).unwrap());
 }
