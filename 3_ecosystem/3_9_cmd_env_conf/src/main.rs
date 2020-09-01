2e216973e76586498c5247114df8219dc6b3675c
ilslv <ilya.solovyiov@gmail.com>
2020-09-01 11:55:44 +0300

    Implement Step 3.9

--- 3_ecosystem/3_9_cmd_env_conf/src/main.rs
+++ 3_ecosystem/3_9_cmd_env_conf/src/main.rs
@@ -1,46 +1,118 @@
+use std::borrow::Cow;
+use std::time::Duration;
+
 use clap::{load_yaml, App};
 use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
-use std::borrow::Cow;
+
+#[derive(Debug, Deserialize)]
+struct Settings<'server, 'db> {
+    mode: Mode,
+    server: Server<'server>,
+    db: MySQL<'db>,
+    log: LogApp,
+    background: Background,
+}
+
+impl<'server, 'db> Settings<'server, 'db> {
+    fn new<'a>(file: impl Into<&'a str>) -> Result<Self, ConfigError> {
+        let mut s = Config::new();
+        let filename = "3_ecosystem/3_9_cmd_env_conf/".to_string() + file.into();
+
+        s.set_default("mode.debug", false)?
+            .set_default("server.external_url", "http://127.0.0.1")?
+            .set_default("server.http_port", 8081)?
+            .set_default("server.grpc_port", 8082)?
+            .set_default("server.healthz_port", 10025)?
+            .set_default("server.metrics_port", 9199)?
+            .set_default("db.mysql.host", "127.0.0.1")?
+            .set_default("db.mysql.port", 3306)?
+            .set_default("db.mysql.dating", "default")?
+            .set_default("db.mysql.user", "root")?
+            .set_default("db.mysql.pass", "")?
+            .set_default("db.mysql.connections.max_idle", 30)?
+            .set_default("db.mysql.connections.max_open", 30)?
+            .set_default("log.app.level", "info")?
+            .set_default("background.watchdog.period", "5s")?
+            .set_default("background.watchdog.limit", 10)?
+            .set_default("background.watchdog.lock_timeout", "4s")?;
+
+        s.set("mode.debug", true)?;
+
+        s.merge(File::with_name(&filename))?;
+        s.merge(Environment::with_prefix("conf"))?;
+
+        s.try_into()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct MySQL<'a> {
+    mysql: Database<'a>,
+}
+
+#[derive(Debug, Deserialize)]
+struct LogApp {
+    app: Log,
+}
+
+#[derive(Debug, Deserialize)]
+struct Background {
+    watchdog: Watchdog,
+}
 
 #[derive(Debug, Deserialize)]
 struct Mode {
-    #[serde(default)]
     debug: bool,
 }
 
 #[derive(Debug, Deserialize)]
 struct Server<'a> {
-    #[serde(default = "default_url")]
     external_url: Cow<'a, str>,
-    #[serde(default = "default_http_port")]
     http_port: u16,
-    #[serde(default = "default_grpc_port")]
     grpc_port: u16,
-    #[serde(default = "default_healthz_port")]
     healthz_port: u16,
-    #[serde(default = "default_metrics_port")]
     metrics_port: u16,
 }
 
-fn default_url() -> Cow<str> {
-    "http://127.0.0.1".into()
+#[derive(Debug, Deserialize)]
+struct Database<'a> {
+    host: Cow<'a, str>,
+    port: u16,
+    dating: Cow<'a, str>,
+    user: Cow<'a, str>,
+    pass: Cow<'a, str>,
+    connections: Connections,
 }
 
-fn default_http_port() -> u16 {
-    8081
+#[derive(Debug, Deserialize)]
+struct Connections {
+    max_idle: u16,
+    max_open: u16,
 }
 
-fn default_grpc_port() -> u16 {
-    8082
+#[derive(Debug, Deserialize)]
+struct Log {
+    level: LogLevel,
 }
 
-fn default_healthz_port() -> u16 {
-    10025
+#[derive(Debug, Deserialize)]
+struct Watchdog {
+    #[serde(with = "humantime_serde")]
+    period: Duration,
+    limit: u16,
+    #[serde(with = "humantime_serde")]
+    lock_timeout: Duration,
 }
 
-fn default_metrics_port() -> u16 {
-    9199
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum LogLevel {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
 }
 
 fn main() {
@@ -48,5 +120,10 @@ fn main() {
     let matches = App::from_yaml(yaml).get_matches();
 
     let conf_file = matches.value_of("config").unwrap();
-    dbg!(conf_file);
+
+    let settings = Settings::new(conf_file).unwrap();
+
+    if matches.is_present("debug") {
+        dbg!(settings);
+    }
 }
