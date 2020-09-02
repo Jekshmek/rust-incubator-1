ec9871d255df1b5733e1393facaabf8b32509f8c
ilslv <ilya.solovyiov@gmail.com>
2020-09-02 11:01:55 +0300

    Fix joining too early

--- 3_ecosystem/3_10_threads/src/main.rs
+++ 3_ecosystem/3_10_threads/src/main.rs
@@ -34,20 +34,23 @@ fn main() {
         let matrix = producer.recv();
 
         crossbeam::scope(|scope| {
-            let mut results = [0_u64; 2];
-
-            for res in &mut results {
-                *res = scope
-                    .spawn(|_| {
+            let handles = (0..)
+                .map(|_| {
+                    scope.spawn(|_| {
                         let sum: u64 = matrix
                             .into_par_iter()
                             .map(|x: &[u8; 64]| x.iter().fold(0_u64, |a, b| a + u64::from(*b)))
                             .sum();
                         sum
                     })
-                    .join()
-                    .unwrap();
-            }
+                })
+                .take(2)
+                .collect::<Vec<_>>();
+
+            let results = handles
+                .into_iter()
+                .map(|handle| handle.join().unwrap())
+                .collect::<Vec<_>>();
 
             assert_eq!(results[0], results[1]);
             println!("{}", results[0]);
